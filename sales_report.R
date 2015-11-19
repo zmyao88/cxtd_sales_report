724ad0f2050c7045c0bf677d05f7e991bae5d9d1
zm.yao88@gmail.com <zm.yao88@gmail.com>
2015-11-20 00:57:55 +0800

    handles error

--- sales_report.R
+++ sales_report.R
@@ -26,11 +26,21 @@ coupon_campaign <- tbl(my_db, 'coupon_campaign') %>%
 coupon_discount <- coupon %>% 
     inner_join(coupon_campaign) %>% 
     #mutate(coupon_off_rate = coupon_discount_rate / 100) %>% 
-    collect() %>%
-    group_by(redeem_sales_id) %>% 
-    summarise(coupon_off_rate = 1 - prod(1-coupon_discount_rate/100))
+    collect() #%>%
+#     group_by(redeem_sales_id) %>% 
+#     summarise(coupon_off_rate = 1 - prod(1-coupon_discount_rate/100))
     # select(redeem_sales_id, coupon_off_rate) 
 
+# create fake coupon_discount table
+if (nrow(coupon_discount) == 0){
+    coupon_discount <- data.frame(redeem_sales_id = integer(),
+                                  coupon_off_rate = numeric()
+    )
+}else{
+    coupon_discount <- coupon_discount %>%
+        group_by(redeem_sales_id) %>% 
+        summarise(coupon_off_rate = 1 - prod(1-coupon_discount_rate/100))
+}
 
 # getting other tables
 member_card_df <- tbl(my_db, 'member') %>% select(member_id, member_card_no)
@@ -59,42 +69,49 @@ sales_point_redemption <- tbl(my_db, 'sales_point_redemption') %>%
 # point_issue_redeem <- dplyr::union(sales_point_issue, sales_point_redemption) %>% 
 #                             inner_join(member_point_transaction, by="member_point_transaction_id") 
                             
-
+if (nrow(sales) > 0){
+    report <- sales %>% transmute(member_id, sales_id, shop_id, transaction_datetime,
+                                  original_amount = invoice_original_amount,
+                                  actual_final_amount = sales_settlement_amount,
+                                  total_discount_amount = invoice_chargeable_amount) %>%
+        left_join(sales_point_issue, by = "sales_id") %>%
+        left_join(sales_point_redemption, by = "sales_id") %>%
+        inner_join(member_card_df, by = 'member_id') %>% 
+        collect() %>%
+        left_join(coupon_discount, by = c("sales_id" = 'redeem_sales_id')) %>%
+        mutate(#coupon_discount_amount = original_amount * coupon_off_rate,
+            point_cashredeem_amount = point_transaction_amount) %>%
+        select(#-redeem_sales_id, 
+            -point_transaction_amount) %>% 
+        
+        transmute(member_id, sales_id, shop_id, transaction_datetime,
+                  original_amount = ifelse(is.na(original_amount), 0, original_amount), 
+                  actual_final_amount = ifelse(is.na(actual_final_amount), 0, actual_final_amount), 
+                  total_discount_amount = ifelse(is.na(total_discount_amount), 0, total_discount_amount), 
+                  point_issue = ifelse(is.na(point_issue), 0, point_issue), 
+                  point_redeemed = ifelse(is.na(point_redeemed), 0, point_redeemed), 
+                  coupon_off_rate = ifelse(is.na(coupon_off_rate), 0, coupon_off_rate), 
+                  member_card_no, 
+                  coupon_discount_amount = original_amount * coupon_off_rate,
+                  #coupon_discount_amount = ifelse(is.na(coupon_discount_amount), 0, coupon_discount_amount), 
+                  point_cashredeem_amount = ifelse(is.na(point_cashredeem_amount), 0, point_cashredeem_amount) 
+        )
+    
+    escape.POSIXt <- dplyr:::escape.Date
+    db_insert_into( con = my_db$con, 
+                    table = "sales_report", 
+                    values = report)
+    print('Success!')
+    
+}else{
+    print('no sales today!')
+}
 
 
 
     
-report <- sales %>% transmute(member_id, sales_id, shop_id, transaction_datetime,
-                        original_amount = invoice_original_amount,
-                        actual_final_amount = sales_settlement_amount,
-                        total_discount_amount = invoice_chargeable_amount) %>%
-                left_join(sales_point_issue, by = "sales_id") %>%
-                left_join(sales_point_redemption, by = "sales_id") %>%
-                inner_join(member_card_df, by = 'member_id') %>% 
-                collect() %>%
-                left_join(coupon_discount, by = c("sales_id" = 'redeem_sales_id')) %>%
-                mutate(#coupon_discount_amount = original_amount * coupon_off_rate,
-                       point_cashredeem_amount = point_transaction_amount) %>%
-                select(#-redeem_sales_id, 
-                       -point_transaction_amount) %>% 
-                
-                transmute(member_id, sales_id, shop_id, transaction_datetime,
-                          original_amount = ifelse(is.na(original_amount), 0, original_amount), 
-                          actual_final_amount = ifelse(is.na(actual_final_amount), 0, actual_final_amount), 
-                          total_discount_amount = ifelse(is.na(total_discount_amount), 0, total_discount_amount), 
-                          point_issue = ifelse(is.na(point_issue), 0, point_issue), 
-                          point_redeemed = ifelse(is.na(point_redeemed), 0, point_redeemed), 
-                          coupon_off_rate = ifelse(is.na(coupon_off_rate), 0, coupon_off_rate), 
-                          member_card_no, 
-                          coupon_discount_amount = original_amount * coupon_off_rate,
-                          #coupon_discount_amount = ifelse(is.na(coupon_discount_amount), 0, coupon_discount_amount), 
-                          point_cashredeem_amount = ifelse(is.na(point_cashredeem_amount), 0, point_cashredeem_amount) 
-                )
+
             
-escape.POSIXt <- dplyr:::escape.Date
-db_insert_into( con = my_db$con, 
-                table = "sales_report", 
-                values = report)
-print('Success!')
+
 # View(report)
 
